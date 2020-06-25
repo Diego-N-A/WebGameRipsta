410acb0cb5b6697335ae8a5c5ccfbcf81b9700fe
Diego Nunes Alves <59403178+Diego-N-A@users.noreply.github.com>
2020-06-25 17:27:55 -0300

    Adicionando os arquivos

---
+++ scripts/jogo/personagem.js
@@ -0,0 +1,39 @@
+class Personagem extends Animacao{
+    constructor(matriz, imagem, x, variacaoY, largura, altura, larguraSprite, alturaSprite){
+        super(matriz, imagem, x, variacaoY, largura, altura, larguraSprite, alturaSprite);
+        this.yIninicial = height - this.altura - this.variacaoY;
+        this.y = this.yIninicial;
+        this.velocidadeDoPulo = 0;
+        this.gravidade = 3;
+        this.alturaDoPulo = -30;
+        this.jumps = 0;
+    }
+    pula() {
+        if (this.jumps < 2) {
+            this.velocidadeDoPulo = this.alturaDoPulo;
+            this.jumps++;
+        }
+    }
+    aplicaGravidade() {
+        this.y = this.y + this.velocidadeDoPulo;
+        this.velocidadeDoPulo = this.velocidadeDoPulo + this.gravidade;
+        if (this.y > this.yIninicial) {
+            this.y = this.yIninicial
+            this.jumps = 0;
+        }
+    }
+    estaColidindo(){
+        const precisao = .7
+        const colisao = collideRectRect(
+            this.x, 
+            this.y, 
+            this.largura * precisao, 
+            this.altura * precisao,
+            inimigo.x,
+            inimigo.y,
+            inimigo.largura * precisao,
+            inimigo.altura * precisao
+        );
+        return colisao;
+    }
+}
