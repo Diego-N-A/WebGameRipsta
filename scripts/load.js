d8c8b9360920c26a5c1e2e3ad87365265349c727
The-coder-ai <dna.spa.2004@gmail.com>
2020-06-25 22:04:37 -0300

    refatorando o código

---
+++ scripts/load.js
@@ -0,0 +1,10 @@
+function preload() {
+    imagemGameOver = loadImage('assets/imagens/assets/game-over.png');
+    imagemCenario = loadImage('assets/imagens/cenario/floresta.png');
+    imagemPersonagem = loadImage('assets/imagens/personagem/correndo.png');
+    imagemInimigo = loadImage('assets/imagens/inimigos/gotinha.png');
+    imagemInimigoVoador = loadImage('assets/imagens/inimigos/gotinha-voadora.png');
+    imagemInimigoGrande = loadImage('assets/imagens/inimigos/troll.png');
+    somDoJogo = loadSound('assets/sons/trilha_jogo.mp3');
+    somDoPulo = loadSound('assets/sons/somPulo.mp3');
+}
